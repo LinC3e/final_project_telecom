e5f6f22eb440ee3ae46bfa7951f1cfb5a654e636
MaTu <ivancostilla17@gmail.com>
2022-10-30 16:35:50 -0300

    Authentificated session + alerts

--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const routerPost = express.Router()
 const { getPosts, showPost, deletePost, newPost, createPost, showFormEditPost, traerPostCard } = require('../controllers/posts')
+const isAuthenticated = require('../middlewares/isauthenticated')
 
 // ruta index
 routerPost.get('/', traerPostCard)
-routerPost.get('/posts',getPosts )
-routerPost.get('/posts/new', newPost)
-routerPost.get('/posts/edit/:id', showFormEditPost)
-routerPost.get('/posts/:slug', showPost)
-routerPost.post('/posts', createPost)
-routerPost.delete('/posts/:id', deletePost)
+routerPost.get('/posts', isAuthenticated, getPosts )
+routerPost.get('/posts/new', isAuthenticated, newPost)
+routerPost.get('/posts/edit/:id',isAuthenticated, showFormEditPost)
+routerPost.get('/posts/:slug',isAuthenticated, showPost)
+routerPost.post('/posts', isAuthenticated ,createPost)
+routerPost.delete('/posts/:id', isAuthenticated ,deletePost)
 
 
 module.exports = {
